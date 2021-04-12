e02d636ff30c661b026909ec49724d4780ebd331
Matthew Lugg <mlugg@mlugg.co.uk>
2021-04-12 18:01:07 +0100

    Simplify split structure

--- io.c
+++ io.c
@@ -44,16 +44,10 @@ static ssize_t _read_splits(FILE *f, unsigned *id, unsigned level, struct split 
 			splits = realloc(splits, splits_alloc * sizeof splits[0]);
 		}
 
-		char *name;
-		uint64_t best = UINT64_MAX;
-		
-		int match = sscanf(line + ntabs, "\"%m[^\n\"]\" %"PRIu64, &name, &best);
-
-		if (match < 1) {
-			free(line);
-			free_splits(splits, nsplits);
-			return -1;
-		}
+		size_t len = strlen(line + ntabs);
+		char *name = malloc(len);
+		strncpy(name, line + ntabs, len - 1);
+		name[len - 1] = 0;
 
 		char *next = NULL;
 
@@ -68,18 +62,11 @@ static ssize_t _read_splits(FILE *f, unsigned *id, unsigned level, struct split 
 			splits[nsplits].split.times = (struct times){
 				.cur = UINT64_MAX,
 				.pb = UINT64_MAX,
-				.best = best,
+				.best = UINT64_MAX,
 				.golded_this_run = false,
 			};
 		} else {
 			// We have subsplits, so it's a group
-			if (match != 1) {
-				// Having times doesn't make sense in a group
-				if (next) free(next);
-				free(line);
-				free_splits(subsplits, nsubsplits);
-				return -1;
-			}
 			splits[nsplits].is_group = true;
 			splits[nsplits].group.expanded = false;
 			splits[nsplits].group.nsplits = nsubsplits;
